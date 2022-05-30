1dff29462e082d5ff302de8fe56cfc82bc917778
Manoj <amnishad0512@gmail.com>
2022-05-30 08:33:57 +0530

    functionality completed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import "./App.css";
 function App() {
   const [number, setNumber] = useState(1);
 
-  // generate RGB values between 0 - 255
+  // generate random RGB values
   function generateColor() {
-    let min = 0,
-      max = 255;
-    let difference = max - min;
-    let rand = Math.random();
-    rand = Math.floor(rand * difference);
-    rand = rand + min;
-    return rand;
+    let value = Math.ceil(Math.random() * 255);
+    return value;
   }
 
   // assign random background color to child items
-  function GenerateRandom() {
+  function assignColor() {
     let firstItem = document.querySelector(".first");
     let secondItem = document.querySelector(".second");
     let thirdItem = document.querySelector(".third");
@@ -24,25 +19,38 @@ function App() {
     let fifthItem = document.querySelector(".fifth");
 
     for (let i = 1; i <= 5; i++) {
+      let red = generateColor();
+      let green = generateColor();
+      let blue = generateColor();
+
       switch (i) {
         case 1: {
-          firstItem.style.backgroundColor = `rgb(${generateColor()}, ${generateColor()}, ${generateColor()})`;
+          // assign color to first item
+          firstItem.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
           break;
         }
+
         case 2: {
-          secondItem.style.backgroundColor = `rgb(${generateColor()}, ${generateColor()}, ${generateColor()})`;
+          // assign color to second item
+          secondItem.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
           break;
         }
+
         case 3: {
-          thirdItem.style.backgroundColor = `rgb(${generateColor()}, ${generateColor()}, ${generateColor()})`;
+          // assign color to third item
+          thirdItem.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
           break;
         }
+
         case 4: {
-          fourthItem.style.backgroundColor = `rgb(${generateColor()}, ${generateColor()}, ${generateColor()})`;
+          // assign color to fourth item
+          fourthItem.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
           break;
         }
+
         default: {
-          fifthItem.style.backgroundColor = `rgb(${generateColor()}, ${generateColor()}, ${generateColor()})`;
+          // assign color to fifth item
+          fifthItem.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
           break;
         }
       }
@@ -50,32 +58,36 @@ function App() {
   }
 
   useEffect(() => {
-    GenerateRandom();
+    assignColor();
   }, []);
 
-  // When the changes happen inside input box it will track that changes
+  // When the changes happen inside the input box it will track that changes
   function handleChange(e) {
     const value = +e.target.value;
     setNumber(value);
   }
 
-  // When we click to any items inside the left container, It will responsive to dispatch item to their original position in the center container
+  // When we click to any items inside of the left container, It will responsible to dispatch item to their original position inside the center container
   function handleItem(e) {
     let className = e.target.className;
     let node = document.getElementsByClassName(className)[0];
     let childCenter = document.querySelector(".child-center");
     let noOfChildren = document.querySelector(".child-center").children.length;
+    let ref = childCenter.firstChild;
 
     if (className === "first") {
+
+      // When we click on first item
       if (noOfChildren) {
-        let ref = childCenter.firstChild;
         childCenter.insertBefore(node, ref);
       } else {
         childCenter.appendChild(node);
       }
+
     } else if (className === "second") {
+
+      // When we click on second item
       if (noOfChildren) {
-        let ref = childCenter.firstChild;
         let className = childCenter.firstChild.className;
         if (className === "first") {
           ref.parentNode.insertBefore(node, ref.nextSibling);
@@ -85,9 +97,11 @@ function App() {
       } else {
         childCenter.appendChild(node);
       }
+
     } else if (className === "third") {
+      // When we click on third item
+
       if (noOfChildren) {
-        let ref = childCenter.firstChild;
         let className = childCenter.firstChild.className;
         if (className === "first") {
           if (!ref.nextSibling) {
@@ -95,7 +109,6 @@ function App() {
           } else {
             ref.parentNode.insertBefore(node, ref.nextSibling.nextSibling);
           }
-          // ref.parentNode.insertBefore(node, ref.nextSibling);
         } else if (className === "second") {
           ref.parentNode.insertBefore(node, ref.nextSibling);
         } else {
@@ -104,9 +117,11 @@ function App() {
       } else {
         childCenter.appendChild(node);
       }
+
     } else if (className === "forth") {
+
+      // When we click on fourth item
       if (noOfChildren) {
-        let ref = childCenter.lastChild;
         let className = childCenter.lastChild.className;
         if (className === "fifth") {
           childCenter.insertBefore(node, ref);
@@ -116,13 +131,17 @@ function App() {
       } else {
         childCenter.appendChild(node);
       }
+
     } else {
+
+      // When we click on fifth item
       childCenter.appendChild(node);
+      
     }
   }
-
-  // When we enter any number between 1 to 5 inside input box then click to shoot button, It will responsible for append that particular item to inside the left container
   function handleButton() {
+    // When we enter any number between 1 to 5 inside input box then click to shoot button, It will responsible for append that particular item to inside the left container
+
     switch (number) {
       case 1: {
         const item = document.querySelector(".first");
@@ -130,24 +149,28 @@ function App() {
         div.append(item);
         break;
       }
+
       case 2: {
         const item = document.querySelector(".second");
         const div = document.querySelector(".child-left");
         div.append(item);
         break;
       }
+
       case 3: {
         const item = document.querySelector(".third");
         const div = document.querySelector(".child-left");
         div.append(item);
         break;
       }
+
       case 4: {
         const item = document.querySelector(".forth");
         const div = document.querySelector(".child-left");
         div.append(item);
         break;
       }
+
       case 5: {
         const item = document.querySelector(".fifth");
         const div = document.querySelector(".child-left");
